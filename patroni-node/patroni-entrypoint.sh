4975486299a6b7328a7775ed15d304ba7e9f8ae3
mich.neuy <mich.neuy@mbanq.com>
2025-08-18 20:55:33 +0700

    allow permission postgres USER

--- patroni-node/patroni-entrypoint.sh
+++ patroni-node/patroni-entrypoint.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# fix ownership and re-exec as postgres.
-if [ "$(id -u)" = "0" ]; then
-  echo "[entrypoint] Detected root. Adjusting ownership and dropping privileges."
-  chown -R postgres:postgres /var/lib/postgresql
-  exec gosu postgres "$0" "$@"
-fi
-
 : "${NODE_NAME:?NODE_NAME not set}"
 : "${PATRONI_SCOPE:?PATRONI_SCOPE not set}"
 : "${ETCD_NODES:?ETCD_NODES not set}"
@@ -16,6 +9,10 @@ fi
 : "${APP_USER:?APP_USER not set}"
 : "${APP_PASSWORD:?APP_PASSWORD not set}"
 : "${POSTGRESQL_DATA_DIR:=/var/lib/postgresql/data}"
+
+echo "[entrypoint] Ensuring ownership of $POSTGRESQL_DATA_DIR is set to postgres:postgres"
+chown -R postgres:postgres /var/lib/postgresql
+chmod 700 -R /var/lib/postgresql/data/
 
 CONFIG_TEMPLATE="/etc/patroni/patroni.yml.template"
 CONFIG_RENDERED="/etc/patroni/patroni.yml"
